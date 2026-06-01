6e75a9cad7395bbdd94aed6346c0225e65bdf2f1
ashwini-ah18 <audhutwarashwini@gmail.com>
2026-06-01 11:00:08 +0000

    app

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st 
+import joblib
+import keras
+import pandas as pd 
+
+
+#provide tab title
+st.set_page_config("Deployment Project")
+#provide the page title 
+st.title("Booking Statuse prediction Model")
+#mention description of the problem statement or if you want to mention your name
+st.subheader("This project takes multiple details as input and predicts whether booking status is approved or not")
+st.subheader("By Ashwini Audhutwar")
